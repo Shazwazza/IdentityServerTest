1bfb4cd429743bafe85f0be11e69c93bbbb54869
Shannon <sdeminick@gmail.com>
2015-12-29 18:27:15 +0100

    applies claims transformations

--- src/MyIdentityServer/App_Start/Startup.Auth.cs
+++ src/MyIdentityServer/App_Start/Startup.Auth.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using IdentityModel;
 using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.OpenIdConnect;
 using MyIdentityServer.Core;
@@ -52,33 +55,35 @@ namespace MyIdentityServer.Website
                 {
                     SecurityTokenValidated = n =>
                     {
-                        //Example of "claims transformation", as seen: https://identityserver.github.io/Documentation/docsv2/overview/mvcGettingStarted.html
+                        //Applies "claims transformation", as seen: https://identityserver.github.io/Documentation/docsv2/overview/mvcGettingStarted.html
+                        //So that only the necessary claims are kept for the user's ticket
 
-                        //var id = n.AuthenticationTicket.Identity;
+                        var id = n.AuthenticationTicket.Identity;
 
-                        //// we want to keep first name, last name, subject and roles
-                        //var givenName = id.FindFirst(Constants.ClaimTypes.GivenName);
-                        //var familyName = id.FindFirst(Constants.ClaimTypes.FamilyName);
-                        //var sub = id.FindFirst(Constants.ClaimTypes.Subject);
-                        //var roles = id.FindAll(Constants.ClaimTypes.Role);
+                        // we want to keep first name, last name, subject and roles
+                        // TODO: maybe more, email?
+                        var givenName = id.FindFirst(JwtClaimTypes.GivenName);
+                        var familyName = id.FindFirst(JwtClaimTypes.FamilyName);
+                        var sub = id.FindFirst(JwtClaimTypes.Subject);
+                        var roles = id.FindAll(JwtClaimTypes.Role);
 
-                        //// create new identity and set name and role claim type
-                        //var nid = new ClaimsIdentity(
-                        //    id.AuthenticationType,
-                        //    Constants.ClaimTypes.GivenName,
-                        //    Constants.ClaimTypes.Role);
+                        // create new identity and set name and role claim type
+                        var nid = new ClaimsIdentity(
+                            id.AuthenticationType,
+                            ClaimTypes.GivenName,
+                            ClaimTypes.Role);
 
-                        //nid.AddClaim(givenName);
-                        //nid.AddClaim(familyName);
-                        //nid.AddClaim(sub);
-                        //nid.AddClaims(roles);
+                        nid.AddClaim(givenName);
+                        nid.AddClaim(familyName);
+                        nid.AddClaim(sub);
+                        nid.AddClaims(roles);
 
-                        //// add some other app specific claim
-                        //nid.AddClaim(new Claim("app_specific", "some data"));
+                        // add some other app specific claim
+                        nid.AddClaim(new Claim("app_specific", "some data"));
 
-                        //n.AuthenticationTicket = new AuthenticationTicket(
-                        //    nid,
-                        //    n.AuthenticationTicket.Properties);
+                        n.AuthenticationTicket = new AuthenticationTicket(
+                            nid,
+                            n.AuthenticationTicket.Properties);
                         return Task.FromResult(0);
                     }
                 }
